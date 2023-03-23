10702c3da61e478aa294dec936f20aff8548af8b
OlesyaFedorovskaya <nyamka13@mail.ru>
2023-03-23 19:20:49 +0300

    added 3rd and 4th task item

--- Seminar1/Homework.java
+++ Seminar1/Homework.java
@@ -11,6 +11,7 @@ int i = new Random().nextInt(k); //это кидалка случайных чи
  */
 
  import java.util.Random;
+ import java.util.Arrays;
 
 public class Homework {
     public static void main(String[] args) {
@@ -18,17 +19,54 @@ public class Homework {
         // 1. Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
 
         Random random = new Random();
-        int i = new Random().nextInt(2000);
+        int i = new Random().nextInt(0, 2000);
         System.out.println("Случайное целое число в диапазоне от 0 до 2000 = " + i);
 
         // 2. Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
 
-        int j;
+        int n;
         System.out.println("Число i в двоичном коде = " + Integer.toBinaryString(i));
-        j = Integer.toBinaryString(i).length();
-        System.out.println("Номер старшего значащего бита выпавшего числа = " + j);
+        n = Integer.toBinaryString(i).length();
+        System.out.println("Номер старшего значащего бита выпавшего числа = " + n);
 
-        //
-        
+        // 3. Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE сохранить в массив m1
+
+        int length = 0;
+        for (int j = i; j < Short.MAX_VALUE; j++) {
+            if (j % n == 0) {
+                length++;
+            }
+        }
+        System.out.println("Длина массива от " + i + " до " + Short.MAX_VALUE + " = " + length);
+
+        int[] m1 = new int[length];
+        int k = 0;
+        for (int j = i; j < Short.MAX_VALUE; j++) {
+            if (j % n == 0) {
+                m1[k] = j;
+                k++;
+            }
+        }
+        System.out.println(Arrays.toString(m1));    
+
+        // 4. Найти все некратные n числа в диапазоне от Short.MIN_VALUE до i и сохранить в массив m2
+
+        int length2 = 0;
+        for (int j = Short.MIN_VALUE; j < i; j++) {
+            if (j % n != 0) {
+                length2++;
+            }
+        }
+        System.out.println("Длина массива от " + Short.MIN_VALUE + " до " + i + " = " + length2);
+
+        int[] m2 = new int[length2];
+        int l = 0;
+        for (int j = Short.MIN_VALUE; j < i; j++) {
+            if (j % n != 0) {
+                m2[l] = j;
+                l++;
+            }
+        }
+        System.out.println(Arrays.toString(m2));      
     }
 }
