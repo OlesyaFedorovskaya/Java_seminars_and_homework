ce8e5e46fda350230a25b6d42556318cd59f62a9
OlesyaFedorovskaya <nyamka13@mail.ru>
2023-03-28 06:22:29 +0300

    added information to the Main2

--- Seminar2/Homework2.java
+++ Seminar2/Homework2.java
@@ -1,5 +1,52 @@
 package Seminar2;
-
+/* 
+Напишите программу, чтобы найти вхождение в строке (содержащей все символы другой строки).
+Напишите программу, чтобы проверить, являются ли две данные строки вращением друг друга (вхождение в обратном порядке).
+*Напишите программу, чтобы перевернуть строку с помощью рекурсии.
+Дано два числа, например 3 и 56, необходимо составить следующие строки: 
+3 + 56 = 59 3 – 56 = -53 3 * 56 = 168 Используем метод StringBuilder.append().
+Замените символ “=” на слово “равно”. Используйте методы StringBuilder.insert(),StringBuilder.deleteCharAt().
+Замените символ “=” на слово “равно”. Используйте методы StringBuilder.replace().
+*Сравнить время выполнения пунка 6 со строкой содержащей 10000 символов "=" средствами String и StringBuilder.
+*/
 public class Homework2 {
+    public static void main(String[] args) {
+        
+// Напишите программу, чтобы найти вхождение в строке (содержащей все символы другой строки).
+// Напишите программу, чтобы проверить, являются ли две данные строки вращением друг друга (вхождение в обратном порядке).
+// *Напишите программу, чтобы перевернуть строку с помощью рекурсии.
+
+String str1 = "Пам - парам - папам ";
+String str2 = "Пам - парам";
+String str3 = "марап - маП";
+
+System.out.println(str1.toLowerCase().contains(str2.toLowerCase()));
+
+StringBuilder builder = new StringBuilder();
+builder.append(str3);
+System.out.println(str2.equals(builder.reverse().toString()));
+
+// Дано два числа, например 3 и 56, необходимо составить следующие строки: 
+// 3 + 56 = 59 3 – 56 = -53 3 * 56 = 168 Используем метод StringBuilder.append().
+int a = 3;
+int b = 56;
+StringBuilder builder2 = new StringBuilder();
+System.out.println(builder2.append(3).append(" ").append("+").append(" ").append(56).append(" ")
+                            .append("=").append(" ").append(59).append(" ").append(3).append(" ")
+                            .append("-").append(" ").append(56).append(" ").append("=").append(" ")
+                            .append(-53).append(" ").append(3).append(" ").append("*").append(" ")
+                            .append(56).append(" ").append("=").append(" ").append(168));
+
+// Замените символ “=” на слово “равно”. Используйте методы StringBuilder.insert(),StringBuilder.deleteCharAt().
+
+
+
+// Замените символ “=” на слово “равно”. Используйте методы StringBuilder.replace().
+
+
+
+// *Сравнить время выполнения пунка 6 со строкой содержащей 10000 символов "=" средствами String и StringBuilder.
+
+    }
     
 }
